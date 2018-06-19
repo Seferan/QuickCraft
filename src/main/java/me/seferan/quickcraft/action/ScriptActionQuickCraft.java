ef22051e01c111c48eae19e021eaa551d1513380
Seferan <seferan@gmail.com>
2018-06-19 16:28:25 -0500

    Added Check if inventory contains required items.

--- src/main/java/me/seferan/quickcraft/action/ScriptActionQuickCraft.java
+++ src/main/java/me/seferan/quickcraft/action/ScriptActionQuickCraft.java
@@ -1,5 +1,9 @@
 package me.seferan.quickcraft.action;
 
+import java.lang.reflect.Field;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+
 import me.seferan.quickcraft.util.ModuleInfo;
 import net.eq2online.console.Log;
 import net.eq2online.macros.scripting.api.APIVersion;
@@ -10,11 +14,12 @@ import net.eq2online.macros.scripting.api.IScriptActionProvider;
 import net.eq2online.macros.scripting.api.ReturnValue;
 import net.eq2online.macros.scripting.parser.ScriptAction;
 import net.eq2online.macros.scripting.parser.ScriptContext;
+import net.eq2online.util.Game;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiCrafting;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.gui.recipebook.GuiRecipeBook;
-import net.minecraft.inventory.Container;
+import net.minecraft.client.util.RecipeItemHelper;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
@@ -22,16 +27,14 @@ import net.minecraft.util.ResourceLocation;
 @APIVersion(ModuleInfo.API_VERSION)
 public class ScriptActionQuickCraft extends ScriptAction {
 
-	public ScriptActionQuickCraft()
-	{
-		super(ScriptContext.MAIN, "quickcraft");		
+	public ScriptActionQuickCraft() {
+		super(ScriptContext.MAIN, "quickcraft");
 	}
-	
-	public void onInit()
-	{
+
+	public void onInit() {
 		this.context.getCore().registerScriptAction(this);
 	}
-	
+
 	public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params) {
 		ReturnValue retVal = new ReturnValue(-1);
 		if (params.length > 0) {
@@ -40,33 +43,43 @@ public class ScriptActionQuickCraft extends ScriptAction {
 			IRecipe recipe = CraftingManager.getRecipe(rs);
 			if (recipe == null) {
 				// handle case where invalid recipe is passed in.
-				Log.info("Invalid Recipe");
+				Game.addChatMessage("Unknown Recipe: " + recipeName);
 				return retVal;
 			}
 
 			GuiContainer gc = this.slotHelper.getGuiContainer();
+			GuiRecipeBook gcrecipebook = null;
 			if (gc != null && gc instanceof GuiCrafting) {
 				GuiCrafting gccrafting = (GuiCrafting) gc;
-				Container inventorySlots = gccrafting.inventorySlots;
-				GuiRecipeBook gcrecipebook = gccrafting.func_194310_f();
-
+				gcrecipebook = gccrafting.func_194310_f();
 			} else if (gc != null && gc instanceof GuiInventory) {
 				if (!recipe.canFit(2, 2)) {
 					// handle case where recipe requires 3x3 grid.
-					Log.info("Crafting Grid too small for recipe!");
+					Game.addChatMessage("Recipe requires crafting table. (3x3)");
 					return retVal;
 				}
 				GuiInventory gcinv = (GuiInventory) gc;
-				GuiRecipeBook gcrecipebook = gcinv.func_194310_f();
+				gcrecipebook = gcinv.func_194310_f();
 			} else {
 				// handle case where we don't either of those crafting windows open.
-				Log.info("Invalid Crafting Window");
+				return retVal;
+			}
+
+			RecipeItemHelper mystackedContents = new RecipeItemHelper();
+			// the following false parameter indicates whether to include "offhand".  We do not want this.
+			mc.player.inventory.fillStackedContents(mystackedContents, false);
+			Boolean canCraftItem = mystackedContents.canCraft(recipe, null);
+			if (!canCraftItem) {
+				// handle case the RecipeItemHelper says we can't craft.
+				// this is likely due to not having the required items.
+				Game.addChatMessage("Unable to craft item: " + recipeName);
 				return retVal;
 			}
 
 			// Perform actual call to "click" on the recipe in the recipe book.
 			// borrowed from mouseClicked of GuiRecipeBook.class
 			this.mc.playerController.func_194338_a(this.mc.player.openContainer.windowId, recipe, true, this.mc.player);
+
 		}
 		return retVal;
 	}
